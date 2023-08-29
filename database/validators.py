73e448d5618ce7db4f36a0f39b78888ef047ab84
Pedro H <pedro_gonsalves@hotmail.com>
2023-08-29 15:27:09 -0300

    add: championship json request validation

--- database/validators.py
+++ database/validators.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
+
 class Validators:
+    actualYear = int(datetime.now().year)
 
     @classmethod
     def stadium_validator(cls, stadium_infos: dict[str:str]) -> dict[str:str]:
@@ -50,6 +54,37 @@ class Validators:
             "code": "000" if codeTeam is None else codeTeam,
             "country": countryTeam,
             "founded": 0000 if foundedTeam is None else foundedTeam
+        }
+
+        return dictToReturn
+
+    @classmethod
+    def champ_validator(cls, champ_infos: dict[str:dict[str:str]]) -> dict[str:str]:
+
+        idChamp = champ_infos.get('league').get('id')
+        nameChamp = champ_infos.get('league').get('name')
+        typeChamp = champ_infos.get('league').get('type')
+        logoChamp = champ_infos.get('league').get('logo')
+        countryChamp = champ_infos.get('country').get('name')
+
+        seasonsInformation: list[dict[str:str]] = champ_infos.get('seasons')
+        for seasonInfo in seasonsInformation:
+            if seasonInfo.get('year') == cls.actualYear:
+                startChamp = seasonInfo.get('start')
+                endChamp = seasonInfo.get('end')
+            else:
+                startChamp = f'{cls.actualYear}-01-01'
+                endChamp = f'{cls.actualYear}-01-01'
+
+        dictToReturn = {
+            "id": idChamp,
+            "name": nameChamp,
+            "logo": logoChamp,
+            "type": typeChamp,
+            "country": countryChamp,
+            "start": startChamp,
+            "end": endChamp,
+            "season": cls.actualYear
         }
 
         return dictToReturn
