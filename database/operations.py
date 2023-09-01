5f7ce7306e9be98c80d49731bb98dfde945e8900
unknown <pedro_gonsalves@hotmail.com>
2023-09-01 17:01:53 -0300

    fix: change methods name because conflicts. add function to treat connection with database

--- database/operations.py
+++ database/operations.py
@@ -1,34 +1,24 @@
-from database.database import DatabaseConnection
-from typing import NoReturn
+from database.connection import DatabaseConnection
+from typing import NoReturn, Tuple, Any
 from database.validators import Validators
+from database.queries import Queries
 
 
 class Operations:
-    validator = Validators()
+    def __init__(self):
+        self.validator = Validators()
+        self.queries = Queries()
+        self.__connection = DatabaseConnection().connection
+        self.__cursor = self.__connection.cursor()
 
-    @classmethod
-    def insert_stadium(cls, stadium_infos: dict[str:str]) -> NoReturn:
+    def stadium_management(self, stadium_infos: dict[str:str]) -> NoReturn:
 
         connection = DatabaseConnection().connection
         cursor = connection.cursor()
 
-        stadiumInfosTreated: dict = cls.validator.stadium_validator(stadium_infos)
+        stadiumInfosTreated: dict = self.validator.stadium_validator(stadium_infos)
 
-        idExStadium = stadiumInfosTreated.get('id')
-        nameStadium = stadiumInfosTreated.get("name")
-        stateStadium = stadiumInfosTreated.get('state')
-        cityStatium = stadiumInfosTreated.get('city')
-        addressStatium = stadiumInfosTreated.get('address')
-        capacityStatium = stadiumInfosTreated.get('capacity')
-        surfaceStadium = stadiumInfosTreated.get('surface')
-        imageStadium = stadiumInfosTreated.get('image')
-
-        queryInsert = f"""
-        
-        insert into "football_data".stadiums (id_stadium, "name", state, city, address, capacity, surface, image)
-        values ({idExStadium}, '{nameStadium}', '{stateStadium}', '{cityStatium}', '{addressStatium}', {capacityStatium}, '{surfaceStadium}', '{imageStadium}')
-        
-        """
+        queryInsert = self.queries.insert_stadium(stadium_values=stadiumInfosTreated)
 
         try:
             cursor.execute(queryInsert)
@@ -40,33 +30,14 @@ class Operations:
         finally:
             connection.close()
 
-    @classmethod
-    def insert_team(cls, team_info: dict[str:any]) -> NoReturn:
+    def team_management(self, team_info: dict[str:any]) -> NoReturn:
 
         connection = DatabaseConnection().connection
         cursor = connection.cursor()
 
-        teamInfos = cls.validator.team_validator(team_info)
+        teamInfos = self.validator.team_validator(team_info)
 
-        idExTeam = teamInfos.get('id')
-        idStadium = teamInfos.get('id_stadium')
-        nameTeam = teamInfos.get('name')
-        logoTeam = teamInfos.get('logo')
-        nationalTeam = teamInfos.get('national')
-        codeTeam = teamInfos.get('code')
-        countryTeam = teamInfos.get('country')
-        foundedTeam = teamInfos.get('founded')
-
-        if idStadium is not None:
-            queryInsert = f"""
-            insert into football_data.teams (id_team, id_stadium, code, "name", country, logo, founded, "national") values
-            ({idExTeam}, (select id from football_data.stadiums where id_stadium = {idStadium}), '{codeTeam}', '{nameTeam}', '{countryTeam}', '{logoTeam}', {foundedTeam}, {nationalTeam})       
-            """
-        else:
-            queryInsert = f"""
-                        insert into football_data.teams (id_team, code, "name", country, logo, founded, "national") values
-                        ({idExTeam}, '{codeTeam}', '{nameTeam}', '{countryTeam}', '{logoTeam}', {foundedTeam}, {nationalTeam})       
-                        """
+        queryInsert = self.queries.insert_team(team_values=teamInfos)
 
         try:
             cursor.execute(queryInsert)
@@ -78,38 +49,78 @@ class Operations:
         finally:
             connection.close()
 
-    @classmethod
-    def insert_champ(cls, champ_infos:dict[str, dict]):
+    def champ_management(self, champ_infos: dict[str, dict]):
 
         connection = DatabaseConnection().connection
         cursor = connection.cursor()
 
-        champInfos = cls.validator.champ_validator(champ_infos)
+        champInfos = self.validator.champ_validator(champ_infos)
 
-        idChamp = champInfos.get("id")
-        nameChamp = champInfos.get('name')
-        startChamp = champInfos.get('start')
-        endChamp = champInfos.get('end')
-        logoChamp = champInfos.get('logo')
-        countryChamp = champInfos.get('country')
-        typeChamp = champInfos.get('type')
-        seasonChamp = champInfos.get('season')
-
-        queryInsert = f"""
-        insert into football_data.champs (id_champ,"name",country,"type",season,start_champ,end_champ,logo)
-        values ({idChamp},'{nameChamp}','{countryChamp}','{typeChamp}',{seasonChamp},'{startChamp}','{endChamp}','{logoChamp}')
-        """
+        queryInsert = self.queries.insert_champ(champInfos)
 
         try:
             cursor.execute(queryInsert)
             connection.commit()
 
-        except Exception as error:
-            print(error)
+        except:
+            connection.rollback()
+
+        finally:
+            connection.close()
+
+    def player_management(self, player_values: dict):
+
+        connection = DatabaseConnection().connection
+        cursor = connection.cursor()
+
+        playerInfos = self.validator.player_validator(player_values)
+        queryInsert = self.queries.insert_player(playerInfos)
+
+        try:
+            cursor.execute(queryInsert)
+            connection.commit()
+
+        except:
             connection.rollback()
 
         finally:
             connection.close()
 
 
-dbOperations = Operations
+    def __perform_consult_query__(self, statement: str) -> Any:
+        try:
+            self.__cursor.execute(statement)
+            resultQuery = self.__cursor.fetchall()
+        except:
+            self.__connection.rollback()
+            resultQuery = None
+        finally:
+            return resultQuery
+
+    def get_all_teams_id(self) -> list[int]:
+
+        queryTeams = self.queries.get_all_team_id
+
+
+        resultQuery = self.__perform_consult_query__(queryTeams)
+        idsList: list[int] = []
+
+        for queryTuple in resultQuery:
+            idsList.append(queryTuple[0])
+
+        return idsList
+
+    def get_all_champs_id(self) -> list[int]:
+
+        queryChamps = self.queries.get_all_champ_id
+
+        resultQuery = self.__perform_consult_query__(queryChamps)
+        idsList: list[int] = []
+
+        for queryTuple in resultQuery:
+            idsList.append(queryTuple[0])
+
+        return idsList
+
+
+dbOperations = Operations()
