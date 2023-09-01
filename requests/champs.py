49bb72bb1725720ad397c754d0c573a46e9b31c9
unknown <pedro_gonsalves@hotmail.com>
2023-09-01 17:04:16 -0300

    add: validation of call file and separe request in two functions

--- requests/champs.py
+++ requests/champs.py
@@ -2,28 +2,33 @@ from typing import NoReturn
 import requests
 from env_var import getHeaders
 from database.operations import dbOperations
-from database.validators import Validators
-
-validator = Validators
 
 
-def makeLeagueRequest():
-    country: str = "Brazil"
+def make_league_request(country: str, camp_type: str) -> list[dict]:
     headers: dict = getHeaders()
+
+    url: str = f"https://v3.football.api-sports.io/leagues?country={country}&type={camp_type}"
+
+    req = requests.get(url, headers=headers)
+    response: list[dict] = req.json()
+    return response
+
+
+def insert_json_champ(champ_request_raw: list[dict]) -> NoReturn:
+    campsInserted: list[int] = []
+    for camp in champ_request_raw:
+        idChamp = camp.get('league').get('id')
+        if idChamp not in campsInserted:
+            dbOperations.insert_champ(camp)
+            campsInserted.append(idChamp)
+
+
+if __name__ == '__main__':
     campTypes: list[str] = ['league', 'cup']
 
+    responseList = []
     for campType in campTypes:
-        url: str = f"https://v3.football.api-sports.io/leagues?country={country}&type={campType}"
+        responseList.append(make_league_request("Brazil", campType))
 
-        req = requests.get(url, headers=headers)
-        response: dict = req.json()
-        camps: list[dict[str, dict]] = response.get('response')
-        campsInserted:list[int] = []
-        for camp in camps:
-            idChamp = camp.get('league').get('id')
-            if idChamp not in campsInserted:
-                dbOperations.insert_champ(camp)
-                campsInserted.append(idChamp)
-
-
-makeLeagueRequest()
+    for jsonChamp in responseList:
+        insert_json_champ(jsonChamp)
