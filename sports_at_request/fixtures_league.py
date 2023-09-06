feaf09f97f11db4015f34377a3d6a0b62b163eda
unknown <pedro_gonsalves@hotmail.com>
2023-09-06 02:12:43 -0300

    add: fixtures by league request

---
+++ sports_at_request/fixtures_league.py
@@ -0,0 +1,25 @@
+from typing import NoReturn
+from sports_at_request.address_request import Request
+from handlers.managers import Managers
+
+req = Request()
+manager = Managers()
+
+
+def insert_json_fixtures(fixtures_request_raw: list[dict[str:dict]]) -> NoReturn:
+    fixtureInserted: list[int] = []
+    for fixture in fixtures_request_raw:
+        idFixture = fixture.get("fixture").get("id")
+        if idFixture not in fixtureInserted:
+            manager.fixture_management(fixture)
+            fixtureInserted.append(idFixture)
+
+
+if __name__ == '__main__':
+    season = "2023"
+    #idChamps = dbOperations.get_all_champs_id()
+    idChamps = [71,73]
+    for idChamp in idChamps:
+        fixturesRequestRaw = req.champ_fixture(id_champ=idChamp,season=season)
+        insert_json_fixtures(fixturesRequestRaw)
+
