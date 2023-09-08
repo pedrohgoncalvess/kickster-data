483a512331905a60301f03a4c4f8f559cb409866
unknown <pedro_gonsalves@hotmail.com>
2023-09-08 16:02:35 -0300

    add: fixture event .py file

---
+++ sports_at_request/fixtures_events.py
@@ -0,0 +1,16 @@
+from handlers.managers import Managers
+from handlers.validators import Validators
+from sports_at_request.address_request import Request
+from database.data_from_db import DataFromDatabase
+
+validator = Validators()
+req = Request()
+manager = Managers()
+dataFromDb = DataFromDatabase()
+
+if __name__ == "__main__":
+    listFixtures = dataFromDb.get_all_fixtures_id()
+
+    for fixtureId in listFixtures:
+        jsonEvents = req.fixtures_events(fixtureId)
+        manager.fixture_event_management(jsonEvents, fixtureId)
