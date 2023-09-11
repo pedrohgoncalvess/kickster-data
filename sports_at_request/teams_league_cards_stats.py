c159163cfbc81c11c387f45ddaf8ccf3504ff3e0
unknown <pedro_gonsalves@hotmail.com>
2023-09-11 20:28:08 -0300

    add: team league cards stats file .py

---
+++ sports_at_request/teams_league_cards_stats.py
@@ -0,0 +1,17 @@
+from handlers.managers import Managers
+from handlers.validators import Validators
+from sports_at_request.address_request import Request
+from database.data_from_db import DataFromDatabase
+
+validator = Validators()
+req = Request()
+manager = Managers()
+dataFromDb = DataFromDatabase()
+
+if __name__ == '__main__':
+    relationTeamsLeague = dataFromDb.get_all_league_teams_id()
+    for teamLeague in relationTeamsLeague:
+        idLeague = relationTeamsLeague.get(teamLeague).get("id_league")
+        idTeam = relationTeamsLeague.get(teamLeague).get("id_team")
+        teamLeagueCardsRawResponse = req.team_league_stats(idTeam, idLeague)
+        manager.team_league_cards_stats_management(teamLeagueCardsRawResponse)
