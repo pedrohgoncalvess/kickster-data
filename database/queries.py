faa755e71b9881ff7d339421fe29646145eb7152
unknown <pedro_gonsalves@hotmail.com>
2023-09-11 02:16:37 -0300

    add: get all id players and fix query get all id leagues

--- database/queries.py
+++ database/queries.py
@@ -4,9 +4,10 @@ from typing import NoReturn
 class Queries:
     def __init__(self):
         self.get_all_team_id = "select id_team from football_data.teams"
-        self.get_all_champ_id = "select id_champ from football_data.champs"
+        self.get_all_league_id = "select id_league from football_data.leagues"
         self.get_all_teams_id_serie_a = "select distinct(team.id_team) as id_team from football_data.fixtures fx inner join football_data.teams team on team.id = fx.id_team_home where fx.id_league = 1"
         self.get_all_fixtures_id = "select id_fixture from football_data.fixtures"
+        self.get_all_id_players_serie_a = "select p.id_player from football_data.teams_squad ts inner join football_data.players p on p.id = ts.id where ts.id_team in (select distinct(team.id) as id_team from football_data.fixtures fx inner join football_data.teams team on team.id = fx.id_team_home where fx.id_league = 1)"
 
     def update_fixture_data_status(self, id_fixture: int) -> NoReturn:
         return f"update football_data.fixtures set data_status = 'collected' where id_fixture = {id_fixture}"
@@ -55,20 +56,20 @@ class Queries:
 
         return queryInsert
 
-    def insert_champ(self, champ_values: dict[str:any]) -> str:
+    def insert_league(self, leagues_values: dict[str:any]) -> str:
 
-        idChamp = champ_values.get("id")
-        nameChamp = champ_values.get('name')
-        startChamp = champ_values.get('start')
-        endChamp = champ_values.get('end')
-        logoChamp = champ_values.get('logo')
-        countryChamp = champ_values.get('country')
-        typeChamp = champ_values.get('type')
-        seasonChamp = champ_values.get('season')
+        idLeague = leagues_values.get("id")
+        nameLeague = leagues_values.get('name')
+        startLeague = leagues_values.get('start')
+        endLeague = leagues_values.get('end')
+        logoLeague = leagues_values.get('logo')
+        countryLeague = leagues_values.get('country')
+        typeLeague = leagues_values.get('type')
+        seasonLeague = leagues_values.get('season')
 
         queryInsert = f"""
-        insert into football_data.champs (id_champ,"name",country,"type",season,start_champ,end_champ,logo)
-        values ({idChamp},'{nameChamp}','{countryChamp}','{typeChamp}',{seasonChamp},'{startChamp}','{endChamp}','{logoChamp}')
+        insert into football_data.leagues (id_league,"name",country,"type",season,start_league,end_league,logo)
+        values ({idLeague},'{nameLeague}','{countryLeague}','{typeLeague}',{seasonLeague},'{startLeague}','{endLeague}','{logoLeague}')
         """
 
         return queryInsert
@@ -111,7 +112,7 @@ class Queries:
             values 
             ({idFixture},
             (select id from football_data.stadiums where id_stadium = {idStadiumFixture}),
-            (select id from football_data.champs where id_champ = {idLeague} and season = {seasonFixture}),
+            (select id from football_data.league where id_league = {idLeague} and season = {seasonFixture}),
             (select id from football_data.teams where id_team = {idHomeTeamFixture}),
             (select id from football_data.teams where id_team = {idAwayTeamFixture}),
             '{dateFixture}',
@@ -126,7 +127,7 @@ class Queries:
             insert into football_data.fixtures (id_fixture,id_league,id_team_home,id_team_away,start_at,result,round,referee,status) 
             values 
             ({idFixture},
-            (select id from football_data.champs where id_champ = {idLeague} and season = {seasonFixture}),
+            (select id from football_data.league where id_league = {idLeague} and season = {seasonFixture}),
             (select id from football_data.teams where id_team = {idHomeTeamFixture}),
             (select id from football_data.teams where id_team = {idAwayTeamFixture}),
             '{dateFixture}',
@@ -254,5 +255,67 @@ class Queries:
                 '{commentEvent}'
                 )
                 """
+
+        return queryInsert
+
+
+    def insert_player_stat(self, player_stat_infos: dict[str:any]) -> str:
+        idPlayer = player_stat_infos.get("id_player")
+        idTeam = player_stat_infos.get("id_team")
+        seasonLeague = player_stat_infos.get("season")
+        idLeague = player_stat_infos.get("id_league")
+        positionPlayer = player_stat_infos.get("position")
+        isCaptain = player_stat_infos.get("captain")
+        gamesAppearances = player_stat_infos.get("appearances")
+        gamesLineUps = player_stat_infos.get("line_ups")
+        gamesMinutes = player_stat_infos.get("minutes")
+        ratingPlayer = player_stat_infos.get("rating")
+        substitutesIn = player_stat_infos.get("substitutes_in")
+        substitutesOut = player_stat_infos.get("substitutes_out")
+        bench = player_stat_infos.get("bench")
+        shotsTotal = player_stat_infos.get("shots_total")
+        shotsOn = player_stat_infos.get("shots_on")
+        goals = player_stat_infos.get("goals")
+        goalsConceded = player_stat_infos.get("goals_conceded")
+        goalsSaved = player_stat_infos.get("goals_saved")
+        assists = player_stat_infos.get("assists")
+        passesTotal = player_stat_infos.get("total_passes")
+        passesKey = player_stat_infos.get("passes_key")
+        passesAccuracy = player_stat_infos.get("accuracy_passes")
+        tacklesTotal = player_stat_infos.get("tackles_total")
+        blocks = player_stat_infos.get("blocks")
+        interceptions = player_stat_infos.get("interceptions")
+        duelsTotal = player_stat_infos.get("duels_total")
+        duelsWin = player_stat_infos.get("duels_win")
+        dribblesAttempted = player_stat_infos.get("dribbles_attempted")
+        dribblesSuccess = player_stat_infos.get("dribbles_success")
+        dribblesPast = player_stat_infos.get("dribbles_past")
+        foulsDraw = player_stat_infos.get("fouls_draw")
+        foulsCommitted = player_stat_infos.get("fouls_committed")
+        cardsYellow = player_stat_infos.get("cards_yellow")
+        cardsYellowRed = player_stat_infos.get("cards_yellowred")
+        cardsRed = player_stat_infos.get("cards_red")
+        penaltiesWon = player_stat_infos.get("penalties_won")
+        penaltiesCommitted = player_stat_infos.get("penalties_committed")
+        penaltiesScored = player_stat_infos.get("penalties_scored")
+        penaltiesMissed = player_stat_infos.get("penalties_missed")
+        penaltiesSaved = player_stat_infos.get("penalties_saved")
+
+        queryInsert = f"""
+        insert into football_data.players_stats (id_player, id_league, id_team, "position", captain, appearances, lineups, "minutes", rating, substitutes_in, 
+        substitutes_out, bench, shots_total, shots_on, goals, assists, conceded_goals, saved_goals, total_pass, key_pass, accuracy_pass, tackles, blocks,
+        interceptions, total_duels, win_duels, attempted_dribbles, success_dribbles, past_dribbles, drawn_fouls, committed_fouls, yellow_cards, yellow_red_cards,
+        red_cards, won_penalties, committed_penalties, scored_penalties, missed_penalty, saved_penalty 
+        ) values (
+        (select id from football_data.players where id_player = {idPlayer}),
+        (select id from football_data.leagues where id_league = {idLeague} and season = {seasonLeague}),
+        (select id from football_data.teams where id_team = {idTeam}), 
+        '{positionPlayer}', {isCaptain}, {gamesAppearances}, {gamesLineUps}, {gamesMinutes}, {ratingPlayer}, {substitutesIn}, {substitutesOut}, 
+        {bench}, {shotsTotal}, {shotsOn}, {goals}, {assists}, {goalsConceded}, {goalsSaved}, {passesTotal}, {passesKey}, 
+        {passesAccuracy}, {tacklesTotal}, {blocks}, {interceptions}, {duelsTotal}, {duelsWin}, {dribblesAttempted},
+        {dribblesSuccess}, {dribblesPast}, {foulsDraw}, {foulsCommitted}, {cardsYellow}, {cardsYellowRed}, {cardsRed}, {penaltiesWon},
+        {penaltiesCommitted}, {penaltiesScored}, {penaltiesMissed}, {penaltiesSaved}
+        )
+        """
 
         return queryInsert
