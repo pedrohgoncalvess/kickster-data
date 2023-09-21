5ed471712678994e3e23c833324810e06463be79
Pedro H <pedro_gonsalves@hotmail.com>
2023-09-21 03:16:31 -0300

    add: team home, team away foreign key with fixture table. refac stadium foreign key

--- models/teams_model.py
+++ models/teams_model.py
@@ -7,15 +7,13 @@ from typing import List
 from models.declarative_base import Base
 
 
-
 class Teams(Base):
     __tablename__ = "teams"
     __table_args__ = {"schema": "ftd"}
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    id_stadium: Mapped[Optional[int]] = mapped_column(ForeignKey("ftd.stadiums.id"), nullable=False)
-    stadiums: Mapped["Stadiums"] = relationship(back_populates="teams")
+    id_stadium: Mapped[Optional[int]] = mapped_column(ForeignKey("ftd.stadiums.id"), nullable=True)
 
     code: Mapped[Optional[str]] = mapped_column(String(3), nullable=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
@@ -23,3 +21,7 @@ class Teams(Base):
     logo: Mapped[Optional[str]] = mapped_column(String(250), nullable=True)
     founded: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
     national: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
+    home_stadium: Mapped["Stadiums"] = relationship(back_populates="stadium_team")
+    fixture_team_home = relationship("Fixtures", back_populates="team_home", foreign_keys="[Fixtures.id_team_home]")
+    fixture_team_away = relationship("Fixtures", back_populates="team_away", foreign_keys="[Fixtures.id_team_away]")
