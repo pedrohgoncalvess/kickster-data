646617c563428aa520e070a54795cb2334ba5c61
Pedro H <pedro_gonsalves@hotmail.com>
2023-09-22 14:57:33 -0300

    add: player_stat fk

--- models/players_model.py
+++ models/players_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, MetaData, Boolean, Integer, Computed, String, Identity, ForeignKey, CHAR
+from sqlalchemy import Numeric, Boolean, Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from typing import Optional, List
@@ -13,12 +13,14 @@ class Players(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    name: Mapped[Optional[str]] = mapped_column(String(150), nullable=False)
-    first_name: Mapped[Optional[str]] = mapped_column(String(150), nullable=True)
-    last_name: Mapped[Optional[str]] = mapped_column(String(150), nullable=True)
-    date_of_birth: Mapped[Optional[types.Date]] = mapped_column(String(40), nullable=True)
+    name: Mapped[Optional[str]] = mapped_column(String(50), nullable=True)
+    first_name: Mapped[Optional[str]] = mapped_column(String(30), nullable=True)
+    last_name: Mapped[Optional[str]] = mapped_column(String(40), nullable=True)
+    date_of_birth: Mapped[Optional[types.Date]] = mapped_column(String(20), nullable=True)
 
     nationality: Mapped[Optional[str]] = mapped_column(String(30), nullable=True)
     height: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
     weight: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
     image: Mapped[Optional[str]] = mapped_column(String(250), nullable=True)
+
+    fk_player_stat: Mapped[List["PlayersStats"]] = relationship(back_populates="player_stat_fk")
