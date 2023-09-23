99c8618e25b1eef5debd54ebfe0f409888fc443b
Pedro H <pedro_gonsalves@hotmail.com>
2023-09-23 02:50:56 -0300

    fix: map with team and league

--- api_requests/teams_goals_stats.py
+++ api_requests/teams_goals_stats.py
@@ -7,9 +7,9 @@ def main():
     manager = Managers()
     dataFromDb = Operations()
 
-    relationTeamsLeague = dataFromDb.get_all_league_teams_id()
+    relationTeamsLeague = dataFromDb.get_relation_team_league_id()
     for teamLeague in relationTeamsLeague:
-        idLeague = relationTeamsLeague.get(teamLeague).get("id_league")
-        idTeam = relationTeamsLeague.get(teamLeague).get("id_team")
+        idLeague = teamLeague[1]
+        idTeam = teamLeague[0]
         teamLeagueGoalsRawResponse = req.team_league_stats(idTeam, idLeague)
         manager.team_league_goals_stats_management(teamLeagueGoalsRawResponse)
