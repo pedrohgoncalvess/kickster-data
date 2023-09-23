7111a90b2c9cd88e7b16cb370e1c36acd02b9c11
Pedro H <pedro_gonsalves@hotmail.com>
2023-09-23 02:54:39 -0300

    add: id_league column and add team fixture stat, team cards stats e team goals stats relation

--- models/leagues_model.py
+++ models/leagues_model.py
@@ -5,13 +5,19 @@ from typing import Optional
 from sqlalchemy.orm import relationship
 from typing import List
 from models.declarative_base import Base
+from models.players_stats_model import PlayersStats
+from models.teams_fixtures_stats_model import TeamsFixturesStats
+from models.teams_cards_stats_model import TeamsCardsStats
+from models.teams_goals_stats_model import TeamsGoalsStats
 
 
 class Leagues(Base):
     __tablename__ = "leagues"
     __table_args__ = {"schema": "ftd"}
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id_league: Mapped[int] = mapped_column(Integer, nullable=False)
+
     name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     country: Mapped[str] = mapped_column(String(15), nullable=False)
     type: Mapped[str] = mapped_column(String(7), nullable=False)
@@ -22,4 +28,7 @@ class Leagues(Base):
 
     league_fixture: Mapped[List["Fixtures"]] = relationship(back_populates="fixture_league")
     league_player_stat_fk: Mapped[List["PlayersStats"]] = relationship(back_populates="player_stat_league_fk")
+    league_team_fixture_stat_fk: Mapped[List["TeamsFixturesStats"]] = relationship(back_populates="team_fixture_stat_league_fk")
+    league_team_cards_stats_fk: Mapped[List["TeamsCardsStats"]] = relationship(back_populates="team_cards_stats_league_fk")
+    league_team_goals_stats_fk: Mapped[List["TeamsGoalsStats"]] = relationship(back_populates="team_goals_stats_league_fk")
 
