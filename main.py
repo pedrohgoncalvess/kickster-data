2dc79e862bb329e326eba5bc01c359a23e1e652a
Pedro H <pedro_gonsalves@hotmail.com>
2023-09-24 14:02:36 -0300

    add: new tables in process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from api_requests import leagues
-from api_requests import teams_and_stadiums
+from api_requests import stadiums
+from api_requests import teams
 from api_requests import players
 from api_requests import fixtures
 from api_requests import fixtures_stats
@@ -13,22 +14,26 @@ from api_requests import teams_fixtures_stats
 from datetime import datetime
 from datetime import timedelta
 
+
 if __name__ == "__main__":
     startedAt = datetime.now()
+    print(f"Starting process... Time: {startedAt}")
     leagues.main()
-    teams_and_stadiums.main()
+    stadiums.main()
+    teams.main()
     print("Starting players table....")
     players.main()
+    teams_squad.main()
+    players_stats.main()
     print("Starting fixtures tables....")
     fixtures.main()
     fixtures_stats.main()
     fixtures_events.main()
     fixtures_lineups.main()
-    teams_squad.main()
-    players_stats.main()
     print("Starting teams stats....")
     teams_cards_stats.main()
     teams_goals_stats.main()
     teams_fixtures_stats.main()
     finishedAt = datetime.now()
-    print(f"Time elapsed: {timedelta(seconds=finishedAt.second, minutes=finishedAt.minute) - timedelta(seconds=startedAt.second, minutes=startedAt.minute)}")
+    print(f"Finishing process... Time: {finishedAt}")
+    print(f"Time elapsed: {timedelta(hours=finishedAt.hour, seconds=finishedAt.second, minutes=finishedAt.minute) - timedelta(seconds=startedAt.second, hours=finishedAt.hour, minutes=startedAt.minute)}")
